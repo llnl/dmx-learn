ed0e5d344db374e963e62d877b3092fcb3122ecd
Walder, Adam Randall <walder2@llnl.gov>
2026-05-13 20:46:48 -0700

    attempt to fix _static error

--- docs/conf.py
+++ docs/conf.py
@@ -31,4 +31,3 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
