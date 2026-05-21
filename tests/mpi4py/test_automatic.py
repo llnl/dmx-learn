b11d483899adf6d7b7a1c2345ef13cac97fe27cc
Walder, Adam Randall <walder2@llnl.gov>
2026-05-21 10:04:41 -0700

    pylint for mpi4py tests

--- tests/mpi4py/test_automatic.py
+++ tests/mpi4py/test_automatic.py
@@ -1,13 +1,14 @@
 """Tests for automatic.py with mpi4py support using 4 cores."""
 
+# pylint: disable=duplicate-code
+
 import os
 import pickle
 
 import numpy as np
 import pytest
-from mpi4py import MPI
+from mpi4py import MPI  # pylint: disable=no-name-in-module
 
-from dmx.bstats import *
 from dmx.mpi4py.utils.automatic import get_dpm_mixture_mpi
 
 DATA_DIR = "tests/data"
@@ -18,7 +19,7 @@ ANSWER_DIR = "tests/answerkeys"
 def test_get_dpm_mixture_mpi(case_id: int) -> None:
     """Tests if pipeline for creating estimator and estiamting a DPM works."""
     comm = MPI.COMM_WORLD
-    world_rank = comm.Get_rank()
+    comm.Get_rank()
 
     with open(os.path.join(DATA_DIR, f"testInput_automatic{case_id}.pkl"), "rb") as f:
         data = pickle.load(f)
@@ -30,6 +31,7 @@ def test_get_dpm_mixture_mpi(case_id: int) -> None:
             ANSWER_DIR, f"testOutput_automatic_get_dpm_mixture_mpi_n4_case{case_id}.txt"
         ),
         "r",
+        encoding="utf-8",
     ) as f:
         answer = f.read()
 
